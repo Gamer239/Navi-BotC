253d8bb617735d24a0c581919d989def8f13f59d
gamer239 <gamer23170@gmail.com>
2015-04-27 17:24:05 -0400

    Increase poll rate of pingers and setup basic serial functionality

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include "stdint.h"
 
 #define CLK 1200000
-#define MAX_TICKS 10000        // Blink length (loop passes)
+#define MAX_TICKS 2500        // Blink length (loop passes)
 #define MAX_RANGE 100
 
 volatile uint16_t pulse_count[3];      //Global Pulse Count
@@ -183,6 +183,7 @@ void InitPorts (void)
   P2OUT |= 0x13;                      // Toggle P2.0 = toggle LED
   P4DIR &= ~0x08;                     // P4.3 = Input
   P4SEL |= 0x08;                      // P4.3 = TB0 = TB compare OUT1
+  P3SEL = 0x30;                       // P3.4,5 = USCI_A0 TXD/RXD
 }
 
 void SetupBasicFunc (void)
@@ -195,6 +196,20 @@ void SetupBasicFunc (void)
   TBCTL   = TASSEL_2 | ID_0 | MC_2;          // SMCLK | Div by 1 | Contin Mode
   TBCCTL0 = CM0 | CM1 | CCIS0 | CAP | SCS | CCIE;  // Ris Edge | Falling Edge | inp = CCI1B | 
                                              // Capture | Sync Cap | Enab IRQ
+  
+  // Config. UART Clock & Baud Rate 
+  BCSCTL1 = CALBC1_1MHZ;                // DCO = 1 MHz
+  DCOCTL  = CALDCO_1MHZ;                // DCO = 1 MHz
+  UCA0CTL1 |= UCSSEL_2;                 // UART use SMCLK
+
+  UCA0MCTL = UCBRS0;                    // Map 1MHz -> 9600 (Tbl 15-4)
+  UCA0BR0  = 104;                       // Map 1MHz -> 9600 (Tbl 15-4)
+  UCA0BR1  = 0;                         // Map 1MHz -> 9600 (Tbl 15-4)
+
+  UCA0CTL1 &= ~UCSWRST;                 // Enable USCI state mach
+
+  while ( !(IFG2 & UCA0TXIFG)) {};      // Confirm that Tx Buff is empty
+  UCA0TXBUF = 0x00;                     // Init robot to stopped state
 
   pulse_count[0] = 0;                           // Init input pulse counter
   dist[0] = 0;                                  //Init distuency
@@ -232,17 +247,20 @@ void CalculateDist( uint8_t ping_num )
 
 void StartPinger( uint8_t ping_num )
 {
+  //left
   if (ping_num == 0)
   {
     P2OUT |= 0x01;                          // Set Pin High P2.0
   }
+  //front
   if (ping_num == 1)
   {
-    P2OUT |= 0x02;
+    P2OUT |= 0x10;
   }
+  //right
   if (ping_num == 2)
   {
-    P2OUT |= 0x10;
+    P2OUT |= 0x02;
   }
   for (i = 100; i != 0; i--) {} ;   // Empty S-Ware delay loop
   if (ping_num == 0)
@@ -251,13 +269,19 @@ void StartPinger( uint8_t ping_num )
   }
   if (ping_num == 1)
   {
-    P2OUT &= ~0x02;
+    P2OUT &= ~0x10;
   }
   if (ping_num == 2)
   {
-    P2OUT &= ~0x10;
+    P2OUT &= ~0x02;
   }
   for (i = MAX_TICKS; i != 0; i--) {} ;   // Empty S-Ware delay loop
+  
+  //have an emtpy loop to let right/left pings disapate
+  if (ping_num != 3)
+  {
+    CalculateDist(ping_num);
+  }
 }
 
 void main(void)
@@ -272,17 +296,15 @@ void main(void)
   SetupBasicFunc();
   CurrentState = 0;
   P1OUT &= ~0x01;
+  uint8_t pinger_sel = 0;
+  
   while(1)
   {
-    StartPinger(0);
-    StartPinger(1);
-    StartPinger(2);
-    CalculateDist(0);
-    CalculateDist(1);
-    CalculateDist(2);
+    StartPinger(pinger_sel);
+    //HallwayLogic(1);
+    pinger_sel++;
+    pinger_sel = pinger_sel % 4;
   }
-
-  //HallwayLogic(1);
-
+  
   _BIS_SR(LPM1_bits + GIE);                  // Enter LPM w/ IRQs enab
 }
