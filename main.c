6670db3605ec93d5a19a834cfbe7866d789d6b5f
Zach Malinowski <zmalinowski@control-tec.com>
2015-04-28 03:15:10 -0400

    walks around a hallway

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include "stdint.h"
 
 #define CLK 1200000
-#define MAX_TICKS 5000        // Blink length (loop passes)
+#define MAX_TICKS 2000        // Blink length (loop passes)
 #define MAX_RANGE 300
 
 volatile uint16_t pulse_count[3];      //Global Pulse Count
@@ -14,6 +14,7 @@ volatile uint16_t cycles[3];
 volatile uint8_t edge[3];
 volatile float pinger[3];
 volatile float history[9];
+volatile uint8_t waiting;
 
 // STATE MACHINE VARIBLES //
 volatile uint8_t CurrentState;
@@ -50,7 +51,7 @@ void TimerReadPinger( uint8_t ping_num )
     fallingEdge[ping_num] = cur_ccr_val;
     if ( fallingEdge[ping_num] < risingEdge[ping_num] )
     {
-          cycles[1] = ( 0xFFFF - risingEdge[ping_num] ) + fallingEdge[ping_num]; //find the cycles to max and then add our current count
+          cycles[ping_num] = ( 0xFFFFFFFF - risingEdge[ping_num] ) + fallingEdge[ping_num]; //find the cycles to max and then add our current count
     }
     //not rollover
     else
@@ -69,12 +70,14 @@ void TimerReadPinger( uint8_t ping_num )
   {
     TBCTL &= 0xFFFE; 				  //reset irq valu
   }
+  
 }
 
 #pragma vector=TIMERA0_VECTOR
 __interrupt void Isrtimera0 (void)
 {
   TimerReadPinger( 0 );
+  waiting = 0;
 }
 
 #pragma vector=TIMERB0_VECTOR
@@ -82,6 +85,7 @@ __interrupt void Isrtimerb0 (void)
 {
   P1OUT |= 0x01;
   TimerReadPinger( 2 );
+  waiting = 0;
 }
 
 #pragma vector=TIMERA1_VECTOR
@@ -96,12 +100,14 @@ __interrupt void IsrCntPulseTACC1 (void)
   {                 
     case TAIV_TACCR1:                 // handle chnl 1 IRQ
         TimerReadPinger( 1 );
+        waiting = 0;
       break;
     case TAIV_TACCR2:                 // ignore chnl 2 IRQ
     case TAIV_TAIFG:                  // ignore TAR rollover IRQ
     default:                          // ignore everything else
       break;
   }
+  
 }
 
 uint8_t MotorController (uint8_t MotorSelect, uint8_t MotorSpeed)
@@ -135,6 +141,124 @@ uint8_t MotorController (uint8_t MotorSelect, uint8_t MotorSpeed)
     }
 }
 
+float VoteForPinger( uint8_t ping_num )
+{
+  float diff1 = history[ping_num*3] - history[ping_num*3+1];
+  float diff2 = history[ping_num*3+1] - history[ping_num*3+2];
+  float diff3 = history[ping_num*3] - history[ping_num*3+2];
+  
+  if (diff1 < 0)
+  {
+    diff1 = diff1 * -1;
+  }
+  
+  if (diff2 < 0)
+  {
+    diff2 = diff2 * -1;
+  }
+  
+  if (diff3 < 0)
+  {
+    diff3 = diff3 * -1;
+  }
+  
+  if ( diff1 < 400 && diff2 < 400 && diff3 < 400 )
+  {
+    return history[ping_num*3];
+  }
+  else if ( diff1 < 400 && diff2 > 400 )
+  {
+    return history[ping_num*3];
+  }
+  else if ( diff1 > 400 && diff2 < 400 )
+  {
+    return history[ping_num*3+2];
+  }
+  else if (diff3 < 400)
+  {
+    return history[ping_num*3];
+  }
+  else
+  {
+    return pinger[ping_num];
+  }
+}
+
+void CalculateDist( uint8_t ping_num )
+{
+  //dist[ping_num] = (float)cycles[ping_num] / (float)CLK;
+  //dist[ping_num] = dist[ping_num] * 1000000;
+  //dist[ping_num] = dist[ping_num] / 12;
+  //dist[ping_num] = dist[ping_num] / 148.;
+  //dist[ping_num] = cycles[ping_num] / 177.6;
+  
+  float dif = pinger[ping_num] - dist[ping_num];
+  if (dif < 0)
+  {
+    dif = dif * -1;
+  }
+  
+ // if (dist[ping_num] > 100 || dist[ping_num] < 1)
+  //{
+    //return;
+  //}
+  
+  //update the history
+  history[ping_num*3] = history[ping_num*3+1];
+  history[ping_num*3+1] = history[ping_num*3+2];
+  history[ping_num*3+2] = cycles[ping_num];
+  
+  pinger[ping_num] = VoteForPinger(ping_num);
+  
+  /*if (dif > 10 && dif < MAX_RANGE && pinger[ping_num] != 0 )
+  {
+    pinger[ping_num] = pinger[ping_num];
+  }
+  else
+  {
+    pinger[ping_num] = dist[ping_num];
+  }*/
+}
+
+void StartPinger( uint8_t ping_num )
+{
+  //left
+  if (ping_num == 0)
+  {
+    P2OUT |= 0x01;                          // Set Pin High P2.0
+  }
+  //front
+  if (ping_num == 1)
+  {
+    P2OUT |= 0x10;
+  }
+  //right
+  if (ping_num == 2)
+  {
+    P2OUT |= 0x02;
+  }
+  for (i = 1000; i != 0; i--) {} ;   // Empty S-Ware delay loop
+  if (ping_num == 0)
+  {
+    P2OUT &= ~0x01;                          // Set Pin Low P2.0
+  }
+  if (ping_num == 1)
+  {
+    P2OUT &= ~0x10;
+  }
+  if (ping_num == 2)
+  {
+    P2OUT &= ~0x02;
+  }
+  for (i = MAX_TICKS; i != 0; i--) {} ;   // Empty S-Ware delay loop
+  
+  //have an emtpy loop to let right/left pings disapate
+  if (ping_num != 3)
+  {
+    CalculateDist(ping_num);
+  }
+}
+
 void HallwayLogic(uint8_t StateMachine)
 //------------------------------------------------------------------------
 // Func:  Run the state machine for the robot
@@ -153,18 +277,31 @@ void HallwayLogic(uint8_t StateMachine)
   if(StateMachine == 1)
   {
     MotorController(0, 32);
-    MotorController(1, 32);
+    MotorController(1, 36);
     P1OUT |= 0x01;                      // Start of TX => toggle LEDs
     P1OUT &= ~0x02;                      // Start of TX => toggle LEDs
   }
   else if(StateMachine == 2)
   {
     TurnCounter++;
+    uint8_t pinger_sel = 0;
     
-    MotorController(0, 40);
-    MotorController(1, 16);
+    if (pinger[1] > 250 && ( history[3] < 1 || history[4] < 1 || history[5] < 1 ) )
+    {
+      //return;
+    }
+    
+    MotorController(0, 5);
+    MotorController(1, 40);
     P1OUT |= 0x02;                      // Start of TX => toggle LEDs
     P1OUT &= ~0x01;                      // Start of TX => toggle LEDs
+    for (i = MAX_TICKS*4.25; i != 0; i--) {} ;   // Empty S-Ware delay loop
+    
+    //MotorController(0, 32);
+    //MotorController(1, 32);
+    
+    //for (i = MAX_TICKS*4; i != 0; i--) {
+    //} ;   // Empty S-Ware delay loop;
   }
   else if(StateMachine == 3)
   {
@@ -227,6 +364,9 @@ void SetupBasicFunc (void)
   pinger[0] = 0;
   pinger[1] = 0;
   pinger[2] = 0;
+  cycles[0] = 0;
+  cycles[1] = 0;
+  cycles[2] = 0;
   fallingEdge[0] = 0;
   risingEdge[0] = 0;
   for(i=0; i < 9; i++)
@@ -241,106 +381,11 @@ void SetupBasicFunc (void)
   _BIS_SR(GIE);                          // IRQs enab
 }
 
-float VoteForPinger( uint8_t ping_num )
-{
-  float diff1 = history[ping_num*3] - history[ping_num*3+1];
-  float diff2 = history[ping_num*3+1] - history[ping_num*3+2];
-  
-  if (diff1 < 0)
-  {
-    diff1 = diff1 * -1;
-  }
-  
-  if (diff2 < 0)
-  {
-    diff2 = diff2 * -1;
-  }
-  
-  if ( diff1 < 10 && diff2 < 10 )
-  {
-    return history[ping_num*3];
-  }
-  else if ( diff1 < 10 && diff2 > 10 )
-  {
-    return history[ping_num*3];
-  }
-  else if ( diff1 > 10 && diff2 < 10)
-  {
-    return history[ping_num*3+2];
-  }
-  else
-  {
-    return pinger[ping_num];
-  }
-}
 
-void CalculateDist( uint8_t ping_num )
-{
-  dist[ping_num] = (float)cycles[ping_num] / (float)CLK;
-  dist[ping_num] = dist[ping_num] * 1000000;
-  dist[ping_num] = dist[ping_num] / 148.;
-  
-  float dif = pinger[ping_num] - dist[ping_num];
-  if (dif < 0)
-  {
-    dif = dif * -1;
-  }
-  
-  //update the history
-  history[ping_num*3] = history[ping_num*3+1];
-  history[ping_num*3+1] = history[ping_num*3+2];
-  history[ping_num*3+2] = dist[ping_num];
-  
-  pinger[ping_num] = VoteForPinger(ping_num);
-  
-  /*if (dif > 10 && dif < MAX_RANGE && pinger[ping_num] != 0 )
-  {
-    pinger[ping_num] = pinger[ping_num];
-  }
-  else
-  {
-    pinger[ping_num] = dist[ping_num];
-  }*/
-}
 
-void StartPinger( uint8_t ping_num )
-{
-  //left
-  if (ping_num == 0)
-  {
-    P2OUT |= 0x01;                          // Set Pin High P2.0
-  }
-  //front
-  if (ping_num == 1)
-  {
-    P2OUT |= 0x10;
-  }
-  //right
-  if (ping_num == 2)
-  {
-    P2OUT |= 0x02;
-  }
-  for (i = 100; i != 0; i--) {} ;   // Empty S-Ware delay loop
-  if (ping_num == 0)
-  {
-    P2OUT &= ~0x01;                          // Set Pin Low P2.0
-  }
-  if (ping_num == 1)
-  {
-    P2OUT &= ~0x10;
-  }
-  if (ping_num == 2)
-  {
-    P2OUT &= ~0x02;
-  }
-  for (i = MAX_TICKS; i != 0; i--) {} ;   // Empty S-Ware delay loop
-  
-  //have an emtpy loop to let right/left pings disapate
-  if (ping_num != 3)
-  {
-    CalculateDist(ping_num);
-  }
-}
+
+
+
 
 void main(void)
 //------------------------------------------------------------------------
@@ -356,47 +401,64 @@ void main(void)
   P1OUT &= ~0x01;
   uint8_t pinger_sel = 0;
   uint8_t j = 0;
+  waiting = 0;
   
-  for (j = 0; j < 4; j++)
+  //while(pinger[0] == 0 && pinger[1] == 0 && pinger[2] == 0)
   {
-    StartPinger(j);
+    //P2OUT ^= 0x03;
+    StartPinger(0);
+    //j++;
+    //j=j%3;
   }
+  
+  //while(1){};
   
   while(1)
   {
     StartPinger(pinger_sel);
     
-    
-    if (pinger[2] < 15)
+    if (pinger[2] < 2655)
     {
       //force stop if we're to close
       HallwayLogic(3);
     }
     //the hallway sensor is reading high
-    else if (pinger[0] > 250  && pinger[2] > 15)
+    else if (pinger[1] > 10620)
     {
       HallwayLogic(2);
     }
     else
     {
       //sweet spot
-      if (pinger[0] > 20 && pinger[0] < 40  && pinger[2] > 15)
+      if (pinger[1] > 3540 && pinger[1] < 8850)
       {
         HallwayLogic(1);
       }
       //to far left
-      else if( pinger[0] < 20 && pinger[2] > 15 )
+      else if( pinger[1] < 3540 )
       {
-        MotorController(0, 36);
+        MotorController(0, 37);
         MotorController(1, 30);
         P1OUT &= ~0x03;                      // Start of TX => toggle LEDs
       }
       //to far right
-      else if ( pinger[0] > 40  && pinger[2] > 15 )
+      else if ( pinger[1] > 7080 )
       {
         MotorController(0, 30);
         MotorController(1, 36);
         P1OUT &= ~0x03;                      // Start of TX => toggle LEDs
+      }
+      else if ( pinger[0] < 10620 )
+      {
+        MotorController(0, 28);
+        MotorController(1, 38);
+        P1OUT &= ~0x03;                      // Start of TX => toggle LEDs
+      }
+      else if (pinger[0] < 1770)
+      {
+        MotorController(0, 40);
+        MotorController(1, 20);
+        P1OUT &= ~0x03;                      // Start of TX => toggle LED
       }
     }
     pinger_sel++;
